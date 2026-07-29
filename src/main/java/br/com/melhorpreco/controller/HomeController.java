a101eb53ef86b7896f5357ddee7bfae30b3048fb
João Gabriel <joaogabrielfk6@gmail.com>
2026-07-29 01:35:35 -0300

    feat: integra página inicial com Thymeleaf

--- src/main/java/br/com/melhorpreco/controller/HomeController.java
+++ src/main/java/br/com/melhorpreco/controller/HomeController.java
@@ -1,12 +1,20 @@
 package br.com.melhorpreco.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("nomeProjeto", "MelhorPreço");
+        model.addAttribute(
+                "descricao",
+                "Compare preços de periféricos e acompanhe suas variações."
+        );
+
         return "home";
     }
 }
